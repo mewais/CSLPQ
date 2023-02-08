900c38ece01d7f4a42c78e97eff2e62e80ac1214
Mohammad Ewais <mewais@users.noreply.github.com>
2023-02-07 23:36:44 -0500

    Some minor fixes by me. I think this is buggy.

--- LFPQ.hpp
+++ LFPQ.hpp
@@ -3,15 +3,15 @@
 #include <random>
 
 template <typename K, typename V>
-class SkipListNode
+class LFPQNode
 {
     private:
         K key;
         V value;
-        std::vector<std::atomic<SkipListNode<K, V>*>> next;
+        std::vector<std::atomic<LFPQNode<K, V>*>> next;
 
     public:
-        SkipListNode(int level, K key, V value)
+        LFPQNode(int level, K key, V value)
             : key(key), value(value), next(level + 1)
         {
             for (int i = 0; i <= level; i++)
@@ -32,25 +32,33 @@ class SkipListNode
 };
 
 template <typename K, typename V>
-class SkipList
+class LFPQ
 {
     private:
-        const int maxLevel = 16;
+        const int max_level = 16;
         std::atomic<int> level;
-        SkipListNode<K, V> *head;
+        LFPQNode<K, V> *head;
 
     public:
-        SkipList()
+        LFPQ()
         {
             this->level = 0;
-            head = new SkipListNode<K, V>(level, K(), V());
+            head = new LFPQNode<K, V>(level, K(), V());
+        }
+
+        int RandomLevel()
+        {
+            static std::mt19937 eng(std::random_device{}());
+            static std::uniform_int_distribution<int> dist(0, this->max_level - 1);
+
+            return dist(eng);
         }
 
         void Push(K key, V value)
         {
-            std::vector<std::atomic<SkipListNode<K, V>*>> update(this->head->next.size());
-            SkipListNode<K, V> *current = this->head;
-            for (int i = this->level; i >= 0; i--)
+            std::vector<std::atomic<LFPQNode<K, V>*>> update(this->head->next.size());
+            LFPQNode<K, V> *current = this->head;
+            for (int i = this->level.load(); i >= 0; i--)
             {
                 while (current->next[i].load() && current->next[i].load()->key < key)
                 {
@@ -61,16 +69,16 @@ class SkipList
             current = current->next[0].load();
             if (current == nullptr || current->key != key)
             {
-                int lvl = randomLevel();
-                if (lvl > this->level)
+                int lvl = RandomLevel();
+                if (lvl > this->level.load())
                 {
-                    for (int i = this->level + 1; i <= lvl; i++)
+                    for (int i = this->level.load() + 1; i <= lvl; i++)
                     {
                         update[i] = this->head;
                     }
                     this->level = lvl;
                 }
-                SkipListNode<K, V> *newNode = new SkipListNode<K, V>(lvl, key, value);
+                LFPQNode<K, V> *newNode = new LFPQNode<K, V>(lvl, key, value);
                 for (int i = 0; i <= lvl; i++)
                 {
                     newNode->next[i].store(update[i]->next[i].load());
@@ -81,11 +89,11 @@ class SkipList
 
         V Pop()
         {
-            SkipListNode<K, V> *current = this->head->next[0].load();
+            LFPQNode<K, V> *current = this->head->next[0].load();
             if (current)
             {
-                std::vector<std::atomic<SkipListNode<K, V>*>> update(this->head->next.size());
-                for (int i = this->level; i >= 0; i--)
+                std::vector<std::atomic<LFPQNode<K, V>*>> update(this->head->next.size());
+                for (int i = this->level.load(); i >= 0; i--)
                 {
                     while (current->next[i].load() && current->next[i].load()->key)
                     {
@@ -94,7 +102,7 @@ class SkipList
                     update[i] = current;
                 }
                 current = current->next[0].load();
-                for (int i = 0; i <= this->level; i++)
+                for (int i = 0; i <= this->level.load(); i++)
                 {
                     update[i]->next[i].store(current->next[i].load());
                 }
@@ -107,18 +115,5 @@ class SkipList
                 return value;
             }
             return V();
-        }
-
-        int randomLevel()
-        {
-            static std::mt19937 eng(std::random_device{}());
-            static std::uniform_int_distribution<int> dist(0, 1);
-
-            int lvl = 0;
-            while (dist(eng) == 0 && lvl < maxLevel)
-            {
-                lvl++;
-            }
-            return lvl;
         }
 };
